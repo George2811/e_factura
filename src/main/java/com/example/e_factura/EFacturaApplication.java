3b2a07a5445c26adef6e0967b945a255022ce3b5
George <sanchez28112001@gmail.com>
2021-10-11 15:32:25 -0500

    added configuration project

---
+++ src/main/java/com/example/e_factura/EFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFacturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFacturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/e_factura/EFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFacturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
