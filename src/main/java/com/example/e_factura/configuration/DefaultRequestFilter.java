0475a7159126dd36fcd3c99249bf11505980661e
George <sanchez28112001@gmail.com>
2021-10-13 22:48:41 -0500

    added DefaultRequestFilter class

--- src/main/java/com/example/e_factura/configuration/DefaultRequestFilter.java
+++ src/main/java/com/example/e_factura/configuration/DefaultRequestFilter.java
@@ -1,5 +1,15 @@
 package com.example.e_factura.configuration;
 
+import com.example.e_factura.domain.service.UserService;
+import com.example.e_factura.util.JwtCenter;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,9 +18,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class DefaultRequestFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    @Autowired
+    private UserService userService;
 
+    @Autowired
+    private JwtCenter tokenCenter;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String token;
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            token = requestTokenHeader.substring(7);
+            tokenCenter.setToken(token);
+
+            try {
+                username = tokenCenter.getUsername();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get JWT Token");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT Token has expired");
+            }
+        } else {
+            logger.warn("JWT Token does not start with Bearer String.");
+        }
+
+        if (username != null && context.getAuthentication() == null) {
+            UserDetails user = this.userService.loadUserByUsername(username);
+
+            if (tokenCenter.validate(user)) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
     }
 }
